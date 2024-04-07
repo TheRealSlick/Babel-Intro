7e3bc8adc93dcf92ccab1687a17aa09669a6e51b
TheRealSlick <wadams0107@gmail.com>
2024-04-07 12:08:50 -0700

    did babel

---
+++ babelProject/lib/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var multiply = function multiply(a, b) {
+  return a * b;
+};
+var firstNum = 2;
+var secondNum = 3;
+var product = multiply(firstNum, secondNum);
+console.log("".concat(firstNum, " times ").concat(secondNum, " = ").concat(product));
---
+++ babelProject/src/index.js
@@ -0,0 +1,6 @@
+const multiply = (a, b) => a * b
+const firstNum = 2
+const secondNum = 3
+let product = multiply(firstNum, secondNum)
+
+console.log(`${firstNum} times ${secondNum} = ${product}`);
